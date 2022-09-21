50ff606725d65fceb114f259885c22c753446c5b
Chris Waddell <yesitschris@hotmail.com>
2022-09-21 15:31:15 +1000

    Directory restructure

--- out.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  // src/scripts/utils/0-general.js
-  var isEmpty = (s) => !s || !s.trim();
-
-  // src/scripts/utils.js
-  console.log(isEmpty({}));
-})();
--- src/scripts/utils/0-general.js
+++
@@ -1,5 +0,0 @@
-const isEmpty = s => !s || !s.trim();
-
-export {
-    isEmpty
-}
